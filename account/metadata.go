51df90ae9913384945b3ea51a11c0c92eb3317a4
Mateusz Pohl <mateusz@dimo.zone>
2025-04-11 01:23:59 +0200

    package for abis

--- account/metadata.go
+++ account/metadata.go
@@ -2,12 +2,14 @@ package account
 
 import (
 	"context"
+	"math/big"
+	"strings"
+
 	"github.com/DIMO-Network/go-zerodev"
+	"github.com/DIMO-Network/go-zerodev/abis"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
-	"math/big"
-	"strings"
 )
 
 type AccountMetadata struct {
@@ -20,42 +22,8 @@ type AccountMetadata struct {
 	Extensions        []*big.Int     `json:"extensions"`
 }
 
-const Eip1271Abi = `[
-    {
-        "type": "function",
-        "name": "eip712Domain",
-        "inputs": [],
-        "outputs": [
-            { "name": "fields", "type": "bytes1", "internalType": "bytes1" },
-            { "name": "name", "type": "string", "internalType": "string" },
-            { "name": "version", "type": "string", "internalType": "string" },
-            { "name": "chainId", "type": "uint256", "internalType": "uint256" },
-            {
-                "name": "verifyingContract",
-                "type": "address",
-                "internalType": "address"
-            },
-            { "name": "salt", "type": "bytes32", "internalType": "bytes32" },
-            { "name": "extensions", "type": "uint256[]", "internalType": "uint256[]" }
-        ],
-        "stateMutability": "view"
-    },
-    {
-        "type": "function",
-        "name": "isValidSignature",
-        "inputs": [
-            { "name": "data", "type": "bytes32", "internalType": "bytes32" },
-            { "name": "signature", "type": "bytes", "internalType": "bytes" }
-        ],
-        "outputs": [
-            { "name": "magicValue", "type": "bytes4", "internalType": "bytes4" }
-        ],
-        "stateMutability": "view"
-    }
-]`
-
 func GetAccountMetadata(client zerodev.RPCClient, address common.Address) (*AccountMetadata, error) {
-	parsedAbi, err := abi.JSON(strings.NewReader(Eip1271Abi))
+	parsedAbi, err := abi.JSON(strings.NewReader(abis.Eip1271Abi))
 	if err != nil {
 		return nil, err
 	}
